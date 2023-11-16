e9e43f05bf4723ec9d95e86e2fbba0d2fc0a0c0c
Matheus Henrique Marques Canuto <1380848@sga.pucminas.br>
2023-11-16 19:55:33 -0300

    Validação dos campos e rotacao dos dados

--- Program.cs
+++ Program.cs
@@ -5,120 +5,170 @@ using System.ComponentModel.Design.Serialization;
 class Program
 {
     static void Main(string[] args)
-    {   
-        //Console.WriteLine("Digite a expressão que você deseja calcular: ");
-        //string expressao = Console.ReadLine();
-        string expressao = "(35+15)/(12-10)*2";
-        /*Definindo um vetor que irá armazenar a expressao Polonesa reversa*/
-        string[] vetorPolonesa = new string[expressao.Length];
-        /*Lista para tratar cada elemento e string expressão*/
-        List<string> elementos = SepararElementos(expressao);
-        /*criando a pilha*/
-        Stack<string> pilha = new Stack<string>();
-        /*vetor de operadores*/
-        string[] vetorOperadores = new string[expressao.Length];
-        int indexPolonesa = -1;
-        int indexOperadores = -1;
-        /*lendo os elementos da lista baseada na expressão*/
-        for (int i=0; i < elementos.Count; i++){
-           
-            //verificando se é número
-            if( int.TryParse(elementos[i],out _)){
-                indexPolonesa++;
-                vetorPolonesa[indexPolonesa]=elementos[i];
-                
-            }else if(elementos[i] == "("){
-                pilha.Push(elementos[i]);
-                Console.WriteLine("Elemento empilhado:" + pilha.Peek());
-            }
-            else if( elementos[i] == "+" || elementos[i] == "-" || elementos[i] == "*" || elementos[i] == "/"){
-                /*se for operador e a prioridade do topo da pilha for maior igual a prioridade 
-                do elemento lido vou desenpilhar e na sequencio empilhar o operador*/
-                while(pilha.Count !=0 && Prioridade(pilha.Peek()) >= Prioridade(elementos[i])){
-                    indexOperadores++;
-                    vetorOperadores[indexOperadores] = pilha.Pop(); 
-                    indexPolonesa++;
-                    vetorPolonesa[indexPolonesa] = vetorOperadores[indexOperadores];
+    {
+        string res = "s";
+        do {
+            Console.WriteLine("Digite a expressão que você deseja calcular: ");
+            string expressao = Console.ReadLine();
+            //string expressao = "(35+15)/(12-10)*2";
+            /*Definindo um vetor que irá armazenar a expressao Polonesa reversa*/
+            bool flag = true;
+            string[] vetorPolonesa = new string[expressao.Length];
+            /*Lista para tratar cada elemento e string expressão*/
+            foreach (char valor in expressao)
+            {
+                if (!(char.IsDigit(valor) || valor == '(' || valor == ')' || valor == '+' || valor == '-' || valor == '*' || valor == '/' || valor == '.'))
+                {
+                    flag = false;
                 }
-                pilha.Push(elementos[i]);
-                Console.WriteLine("Elemento empilhado:" + pilha.Peek());
+            }
+            if (flag)
+            {
+                List<string> elementos = SepararElementos(expressao);
+                /*criando a pilha*/
+                Stack<string> pilha = new Stack<string>();
+                /*vetor de operadores*/
+                string[] vetorOperadores = new string[expressao.Length];
+                int indexPolonesa = -1;
+                int indexOperadores = -1;
+                /*lendo os elementos da lista baseada na expressão*/
+                for (int i = 0; i < elementos.Count; i++)
+                {
 
-            }else if(elementos[i] == ")"){
-                /*encontrado o fechamento de parenteses seginifica que tem que desempilhar até 
-                achar o outro '('*/
-                while(pilha.Count != 0 && pilha.Peek() != "("){
-                    
+                    //verificando se é número
+                    if (float.TryParse(elementos[i], out _))
+                    {
+                        indexPolonesa++;
+                        vetorPolonesa[indexPolonesa] = elementos[i];
+
+                    }
+                    else if (elementos[i] == "(")
+                    {
+                        pilha.Push(elementos[i]);
+                        Console.WriteLine("Elemento empilhado:" + pilha.Peek());
+                    }
+                    else if (elementos[i] == "+" || elementos[i] == "-" || elementos[i] == "*" || elementos[i] == "/")
+                    {
+                        /*se for operador e a prioridade do topo da pilha for maior igual a prioridade 
+                        do elemento lido vou desenpilhar e na sequencio empilhar o operador*/
+                        while (pilha.Count != 0 && Prioridade(pilha.Peek()) >= Prioridade(elementos[i]))
+                        {
+                            indexOperadores++;
+                            vetorOperadores[indexOperadores] = pilha.Pop();
+                            indexPolonesa++;
+                            vetorPolonesa[indexPolonesa] = vetorOperadores[indexOperadores];
+                        }
+                        pilha.Push(elementos[i]);
+                        Console.WriteLine("Elemento empilhado:" + pilha.Peek());
+
+                    }
+                    else if (elementos[i] == ")")
+                    {
+                        /*encontrado o fechamento de parenteses seginifica que tem que desempilhar até 
+                        achar o outro '('*/
+                        while (pilha.Count != 0 && pilha.Peek() != "(")
+                        {
+
+                            indexOperadores++;
+                            vetorOperadores[indexOperadores] = pilha.Pop();
+                            indexPolonesa++;
+                            vetorPolonesa[indexPolonesa] = vetorOperadores[indexOperadores];
+
+                        }
+                        if (pilha.Peek() == "(")
+                        {
+                            pilha.Pop();
+                        }
+                    }
+
+                }
+                /*descarregando o restante dos operadores da pilha no vetor de notação Polonesa*/
+                while (pilha.Count != 0)
+                {
+                    if (pilha.Peek() == "(")
+                    {
+                        pilha.Pop();
+                    }
+                    else
+                    {
                         indexOperadores++;
-                        vetorOperadores[indexOperadores] = pilha.Pop(); 
+                        vetorOperadores[indexOperadores] = pilha.Pop();
                         indexPolonesa++;
                         vetorPolonesa[indexPolonesa] = vetorOperadores[indexOperadores];
-                     
-                }
-                if (pilha.Peek()=="("){
-                    pilha.Pop();
-                }    
-            }  
-            
-        }
-        /*descarregando o restante dos operadores da pilha no vetor de notação Polonesa*/
-        while( pilha.Count != 0){
-            if(pilha.Peek() =="("){
-                pilha.Pop();
-            }else{
-                indexOperadores++;
-                vetorOperadores[indexOperadores] = pilha.Pop(); 
-                indexPolonesa++;
-                vetorPolonesa[indexPolonesa] = vetorOperadores[indexOperadores];
-            }
-            
-        }        
-
-        pilha.Clear();
-        /*lendo a notação polonesa e fazendo as oparções*/
-        for (int j=0; j < vetorPolonesa.Length; j++){ 
-
-            /*se for número empilha*/
-            if(float.TryParse(vetorPolonesa[j],out _)){
-
-                pilha.Push(vetorPolonesa[j]);
-
-            }else{
-                /*verificando se a pilha está vazia*/
-                if (pilha.Count ==0)
-                {
-                    Console.WriteLine("Pilha vazia");
-                }else{
-                    float y = float.Parse(pilha.Pop());
-
-                    if(pilha.Count ==0 ){
-                        Console.WriteLine("Pilha vazia");
-                    }else{
-                        float x = float.Parse(pilha.Pop());
-
-                        switch (vetorPolonesa[j])
-                        {   
-                            case "+": 
-                                pilha.Push(Convert.ToString(x + y));
-                                break;
-                            case "-":
-                                pilha.Push(Convert.ToString(x-y));
-                                break;
-                            case "*":
-                                pilha.Push(Convert.ToString(x*y));
-                                break;
-                            case "/":
-                                pilha.Push(Convert.ToString(x/y));
-                                break;
-
-                            default:
-                                Console.WriteLine("Número inválido!");
-                                break;
-                        }
-                        Console.WriteLine("O resultado da expressão digitada é :" + pilha.Peek());
                     }
-                }   
-            }   
-        }   
+
+                }
+                Console.WriteLine("Expressão polonesa: ");
+                for(int i = 0; i < vetorPolonesa.Length; i++)
+                {
+                    Console.Write(vetorPolonesa[i]);
+                }
+                Console.WriteLine("");
+                pilha.Clear();
+                /*lendo a notação polonesa e fazendo as oparções*/
+                for (int j = 0; j < vetorPolonesa.Length; j++)
+                {
+
+                    /*se for número empilha*/
+                    if (float.TryParse(vetorPolonesa[j], out _))
+                    {
+
+                        pilha.Push(vetorPolonesa[j]);
+
+                    }
+                    else
+                    {
+                        /*verificando se a pilha está vazia*/
+                        if (pilha.Count == 0)
+                        {
+                            Console.WriteLine(" ");
+                        }
+                        else
+                        {
+                            float y = float.Parse(pilha.Pop());
+
+                            if (pilha.Count == 0)
+                            {
+                                Console.WriteLine(" ");
+                            }
+                            else
+                            {
+                                float x = float.Parse(pilha.Pop());
+
+                                switch (vetorPolonesa[j])
+                                {
+                                    case "+":
+                                        pilha.Push(Convert.ToString(x + y));
+                                        break;
+                                    case "-":
+                                        pilha.Push(Convert.ToString(x - y));
+                                        break;
+                                    case "*":
+                                        pilha.Push(Convert.ToString(x * y));
+                                        break;
+                                    case "/":
+                                        pilha.Push(Convert.ToString(x / y));
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("Número inválido!");
+                                        break;
+                                }
+                                Console.WriteLine("O resultado da expressão digitada é :" + pilha.Peek());
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Digite uma expressão válida!");
+            }
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Deseja digitar uma nova expressao? s/n");
+            res = Console.ReadLine();
+        } while (res == "s" || res == "S");
         
     }
     static int Prioridade(string elemento){
@@ -142,7 +192,7 @@ class Program
         foreach (char caractere in expressao)
         {
             /*verificando se é número*/
-            if (char.IsDigit(caractere))
+            if (char.IsDigit(caractere) || caractere == '.')
             {
                 numeroAtual += caractere;
             }
